fa310f7fbb491cd13805046d8c62842a9945a175
Huzaifa <huzafaameen0900@gmail.com>
2025-11-12 19:51:18 +0500

    Pool Update for deployment

--- backend/index.js
+++ backend/index.js
@@ -12,11 +12,8 @@ app.use(cors());
 app.use(express.json());
 
 const pool = new pg.Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
 });
 
 pool.connect((err, client, release) => {
